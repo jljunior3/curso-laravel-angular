2ba98002bfedf7435595838d6783a2a4686c0284
Jonas Leite <jonasleitep4@gmail.com>
2016-02-08 12:51:11 -0200

    Aplicando as camadas: Repositories e Services

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,38 +2,50 @@
 
 namespace CodeProject\Http\Controllers;
 
-use CodeProject\Client;
+use CodeProject\Repositories\ClientRepository;
+use CodeProject\Services\ClientService;
 use Illuminate\Http\Request;
-
-use CodeProject\Http\Requests;
-use CodeProject\Http\Controllers\Controller;
 
 class ClientController extends Controller
 {
+    /**
+     * @var ClientRepository
+     */
+    private $repository;
+
+    /**
+     * @var ClientService
+     */
+    private $service;
+
+    public function __construct(ClientRepository $repository, ClientService $service)
+    {
+        $this->repository = $repository;
+        $this->service = $service;
+    }
+
     public function index()
     {
-        return Client::all();
+        return $this->repository->all();
     }
 
     public function store(Request $request)
     {
-        return Client::create($request->all());
-    }
-
-    public function update($id, Request $request)
-    {
-        Client::find($id)->update($request->all());
-
-        return Client::find($id);
+        return $this->service->create($request->all());
     }
 
     public function show($id)
     {
-        return Client::find($id);
+        return $this->repository->find($id);
+    }
+
+    public function update($id, Request $request)
+    {
+        return $this->service->update($request->all(), $id);
     }
 
     public function destroy($id)
     {
-        Client::findOrFail($id)->delete();
+        $this->repository->delete($id);
     }
 }
